2aaad16d89b136dba8adc516922a90a80ed53b68
whoami <11@qq.com>
2016-05-19 02:53:27 -0700

    test2

--- spider.py
+++ spider.py
@@ -4,5 +4,11 @@ from bs4 import BeautifulSoup
 
 
 html=urllib.urlopen("http://www.wooyun.org/corps/page/44")
-bsobj=BeautifulSoup(html.read())
-print (bsobj.h1)
+try:
+    html=urllib.urlopen("http://www.wooyun.org/corps/page/44")
+    bsobj=BeautifulSoup(html.read())
+    print (bsobj.tbody.tr[1])    
+except Exception,ex:
+    print ex
+    
+
